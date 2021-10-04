d3a649736d3fef4f8b90712363356e367c21eb05
RvJonh <jonhvelasco3@gmail.com>
2021-10-03 21:17:43 -0430

    version 1.0.1

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
-# Getting Started with Create React App
+## Simple Calculator
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is my calculator, which is created to practice the different tecnologies on the web, learn more about math operations, and it's a part proyect for freeCodeCamp curriculum.
+
+## Live link code 
+
+Check a demo here : 
+
+## Deployment
+
+To deploy you need download this repository, open it and install the "create react app" through Node.js
+
+* This proyect uses React
+* The repository "Create React app"
+** Learn more about these tecnologies check below 
+* CSS(SCSS) you can use live Live Sass Compiler v3.0.0 or above ^
+* Javascript
+
+## Getting Started this proyect
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). So if you are not familiar with this tecnology check below.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
 
 ## Available Scripts
 
@@ -27,44 +51,6 @@ It correctly bundles React in production mode and optimizes the build for the be
 The build is minified and the filenames include the hashes.\
 Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
 ### `npm run eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
 ### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/index.js
+++ src/index.js
@@ -1,85 +1,263 @@
-import React,{Component} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
+//symbols allowed in by the calculator "+ - * / .";
+let symbols = /[/*-+-.]/;
+let numbers = /^[0-9]+$/;
+let endsWithOperator = /[x+‑/]$/;
 
-class Calculator extends Component{
-  constructor(props){
-      super(props)
-      this.state ={
-          result : "Result",
-          operation : "0"
-      }
-  }
-  componentDidMount(){
-      document.addEventListener("keydown",this.handlePressing);
-  }
-  componentWillUnmount(){
-      document.removeEventListener("keypress");
-  }
+let limitDigit = 22;
 
-  handleClear = ()=>{
-      this.setState({
-            result : "Result",
-            operation : "0"
-      });
-  }
+class Calculator extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			startedExpresion: false,
+			aux: "",
+			lastOperation: "0",
+			result: "0",
+		};
+	}
+	componentDidMount() {
+		document.addEventListener("keydown", this.handlePressing);
+	}
+	componentWillUnmount() {
+		document.removeEventListener("keydown", this.handlePressing);
+	}
 
-  handlePressing = (e)=>{
-      console.log(e.code);
-  }
+	handleClear = () => {
+		this.setState({
+			startedExpresion: false,
+			aux: "",
+			lastOperation: "0",
+			result: "0",
+		});
+	};
+	handleBackDelete = () => {
+		if (this.state.startedExpresion) {
+			this.setState({
+				lastOperation: this.state.lastOperation.slice(0, this.state.lastOperation.length - 1),
+				result: this.state.result.slice(0, this.state.result.length - 1),
+			});
 
-  handleAdding = (e)=>{
-      this.setState({
-          operation:this.state.operation==="0" ? e.target.innerText : this.state.operation+e.target.innerText
-        })
-  }
+			if(this.state.lastOperation.length === 0){
+				this.setState({
+					lastOperation: "0"
+				})
+			}			
+			if(this.state.result.length === 0){
+				this.setState({
+					result: "0"
+				})
+			}				
 
-  handleResult = ()=>{
-      this.setState({
-        result : this.state.operation
-      })
-  }
+		} else {
+			this.handleClear();
+		}
+	};
 
-  render(){
-      return(
-          <div id="calculator">
-              <div id="display">
-                  <p>{this.state.result}</p>
-                  <p>{this.state.operation}</p>
-              </div>
-              <div id="board">
-                  <button className="button" onClick={this.handleAdding} id="zero">0</button>
-                  <button className="button" onClick={this.handleAdding} id="one">1</button>
-                  <button className="button" onClick={this.handleAdding} id="two">2</button>
-                  <button className="button" onClick={this.handleAdding} id="three">3</button>
-                  <button className="button" onClick={this.handleAdding} id="four">4</button>
-                  <button className="button" onClick={this.handleAdding} id="five">5</button>
-                  <button className="button" onClick={this.handleAdding} id="six">6</button>
-                  <button className="button" onClick={this.handleAdding} id="seven">7</button>
-                  <button className="button" onClick={this.handleAdding} id="eight">8</button>
-                  <button className="button" onClick={this.handleAdding} id="nine">9</button>
+	handlePressing = (e) => {
+		//for instruccions
+		switch (e.key.toLowerCase()) {
+			case "enter":
+				this.handleResult();
+				break;
+			case "delete":
+				this.handleClear();
+				break;
+			case "backspace":
+				this.handleBackDelete();
+				break;
 
+			default:
+			//nothing
+		}
+		//to detect the numbers or the digits
+		if (numbers.test(e.key) || symbols.test(e.key)) {
+			this.handleEvaluate(e.key);
+		}
+	};
+	handleClicking = (e) => {
+		this.handleEvaluate(e.target.innerHTML);
+	};
 
-                  <button className="button" onClick={this.handleAdding} id="add">+</button>
-                  <button className="button" onClick={this.handleAdding} id="subtract">-</button>
-                  <button className="button" onClick={this.handleAdding} id="multiply">x</button>
-                  <button className="button" onClick={this.handleAdding} id="divide">/</button>
+	handleEvaluate = (value) => {
+		// if there is no space for new digits
+		if (this.state.result.length >= limitDigit) {
+			this.limitReached();
+		} else {
+			// attaching new values to the expresiong
+			let expresion = value;
+			expresion = expresion === "*" ? "x" : expresion;
 
-                  <button className="button" onClick={this.handleAdding} id="decimal">.</button>
-                  <button className="button" onClick={this.handleClear} id="clear">AC</button>
-                  <button className="button" onClick={this.handleResult} id="equals">=</button>
+			if (!this.state.startedExpresion) {
+				if (expresion === ".") {
+					if (!this.state.result.includes(".")) {
+						this.setState({
+							startedExpresion: true,
+							lastOperation: this.state.lastOperation + ".",
+							result: this.state.result + ".",
+						});
+					}
 
-              </div>
-          </div>
-      )
-  }
+				} else if (/[1-9]/.test(expresion)) {
+					this.setState({
+						startedExpresion: true,
+						lastOperation: expresion,
+						result: expresion,
+					});
+				} else if (/[+|\-|x|/]/.test(expresion)) {
+					this.setState({
+						startedExpresion: true,
+						lastOperation: this.state.lastOperation + expresion,
+						result: expresion,
+					});
+				}
+			} else {
+				if (expresion === ".") {
+					if (!this.state.result.includes(".")) {
+						if (!/[+x\-/]/.test(this.state.result[this.state.result.length - 1])) {
+							this.setState({
+								lastOperation: this.state.lastOperation + expresion,
+								result: this.state.result + expresion,
+							});
+						} else {
+							console.log("de lokos");
+							this.setState({
+								lastOperation: this.state.lastOperation + "0.",
+								result: this.state.result + "0.",
+							});
+						}
+					}
+				} else if (/[0-9]/.test(expresion)) {
+					this.setState({
+						lastOperation: this.state.lastOperation + expresion,
+						result: this.state.result + expresion,
+					});
+
+				} else if (/[+|x|/]/.test(expresion)) {
+
+					if(/[+|x|/]/.test(this.state.lastOperation[this.state.lastOperation.length -1])){
+						this.setState({
+							lastOperation: this.state.lastOperation.slice(0, this.state.lastOperation.length - 1) + expresion,
+							result: expresion,
+						});
+					}else if(/[-]/.test(this.state.lastOperation[this.state.lastOperation.length -1])){
+						this.setState({
+							lastOperation: this.state.lastOperation.slice(0, this.state.lastOperation.length - 2) + expresion,
+							result: expresion,
+						});
+					}else{
+						this.setState({
+							lastOperation : this.state.lastOperation + expresion,
+							result : expresion
+						})
+						
+					}
+
+				}else if(/[-]/.test(expresion)){
+					if(!/[-]/.test(this.state.lastOperation[this.state.lastOperation.length-1])){
+						this.setState({
+							lastOperation : this.state.lastOperation + expresion,
+							result : expresion
+						});
+					}
+				}
+			}
+		}
+	};
+
+	limitReached = () => {
+		this.setState({
+			aux: this.state.lastOperation,
+			lastOperation: "Limit reached",
+		});
+		setTimeout(() => {
+			this.setState({
+				lastOperation: this.state.aux,
+			});
+		}, 1000);
+	};
+
+	handleResult = () => {
+		if (this.state.startedExpresion) {
+			if(this.state.result[this.state.result.length - 1] === ".") {
+				this.setState({
+					startedExpresion : false,
+					lastOperation: this.operate(this.state.lastOperation + "0"),
+					result: this.operate(this.state.lastOperation + "0"),
+				});
+			}else {
+				this.setState({
+					startedExpresion : false,
+					lastOperation: this.operate(this.state.lastOperation),
+					result: this.operate(this.state.lastOperation),
+				});
+			}
+
+			if(Number.isNaN(this.state.result)){
+				this.setState({
+					lastOperation : "0",
+					result : "0"
+				})
+			}
+
+		}
+	};
+	operate = (formula) => {
+		let expression = formula;
+		while (endsWithOperator.test(expression)) {
+			expression = expression.slice(0, -1);
+		}
+		expression = expression.replace(/x/g, "*").replace(/‑/g, "-").replace("--", "+0+0+0+0+0+0+");
+		let answer;
+		try{
+			answer = Math.round(1000000000000 * eval(expression)) / 1000000000000;
+		}catch{
+			answer = NaN;
+		}
+		
+		return answer.toString();
+	};
+
+	render() {
+		return (
+			<div id="calculator">
+				<div className="panel">
+					<p id="formula">{this.state.lastOperation}</p>
+					<p id="display">{this.state.result}</p>
+				</div>
+
+				<div id="board">
+					<button className="button" onClick={this.handleClicking} id="zero">0</button>
+					<button className="button" onClick={this.handleClicking} id="one">1</button>
+					<button className="button" onClick={this.handleClicking} id="two">2</button>
+					<button className="button" onClick={this.handleClicking} id="three">3</button>
+					<button className="button" onClick={this.handleClicking} id="four">4</button>
+					<button className="button" onClick={this.handleClicking} id="five">5</button>
+					<button className="button" onClick={this.handleClicking} id="six">6</button>
+					<button className="button" onClick={this.handleClicking} id="seven">7</button>
+					<button className="button" onClick={this.handleClicking} id="eight">8</button>
+					<button className="button" onClick={this.handleClicking} id="nine">9</button>
+
+					<button className="button" onClick={this.handleClicking} id="add">+</button>
+					<button className="button" onClick={this.handleClicking} id="subtract">-</button>
+					<button className="button" onClick={this.handleClicking} id="multiply">x</button>
+					<button className="button" onClick={this.handleClicking} id="divide">/</button>
+
+					<button className="button" onClick={this.handleClicking} id="decimal">.</button>
+					<button className="button" onClick={this.handleClear} id="clear">AC</button>
+					<button className="button" onClick={this.handleResult} id="equals">=</button>
+				</div>
+			</div>
+		);
+	}
 }
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <Calculator />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Calculator />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
